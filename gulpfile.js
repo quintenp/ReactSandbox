b6335fa96ebbf6da3b26c5882cac6214c6d309af
Quinten <Quinten.pohl@gmail.com>
2019-02-03 15:54:59 +0000

    browserify config done

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,18 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect'); //web server
 var open = require('gulp-open'); //open url
+var browserify = require('browserify'); //bundle
+var reactify = require('reactify'); //jsx to js
+var source = require('vinyl-source-stream'); //text streams with gulp
 
 var config = {
     port: 9005,
     devBaseUrl: 'http://localhost:9005',
     paths: {
         html: './src/*.html',
-        dist: './dist'
+        js: './src/**/*.js',
+        dist: './dist',
+        mainJs: './src/app.js'
     }
 }
 
@@ -29,18 +34,28 @@ gulp.task('open', gulp.series('connect', function (done) {
     done();
 }));
 
-
 gulp.task('html', function (done) {
     gulp.src(config.paths.html)
         .pipe(gulp.dest(config.paths.dist))
         .pipe(connect.reload());
+    done();
+});
 
+gulp.task('js', function (done) {
+    browserify(config.paths.mainJs)
+        .transform(reactify)
+        .bundle()
+        .on('error', console.error.bind(console))
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest(config.paths.dist + '/scripts'))
+        .pipe(connect.reload());
     done();
 });
 
 gulp.task('watch', function () {
     gulp.watch(config.paths.html, gulp.series('html'));
+    gulp.watch(config.paths.js, gulp.series('js'));
 });
 
-gulp.task('default', gulp.series('html', 'open', 'watch'));
+gulp.task('default', gulp.series('html', 'js', 'open', 'watch'));
 
