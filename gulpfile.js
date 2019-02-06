5bc5bdb4e892dcf73e8095d37b5060d88ae4c832
quinten_pohl <quinten_pohl@dell.com>
2019-02-06 16:55:24 +0000

    gulp tasks completed: lint + css bundle

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ var open = require('gulp-open'); //open url
 var browserify = require('browserify'); //bundle
 var reactify = require('reactify'); //jsx to js
 var source = require('vinyl-source-stream'); //text streams with gulp
+var concat = require('gulp-concat'); //Concats files
+var eslint = require('gulp-eslint'); //Lints the code
 
 var config = {
     port: 9005,
@@ -13,6 +15,9 @@ var config = {
     paths: {
         html: './src/*.html',
         js: './src/**/*.js',
+        css: [
+            'node_modules/bootstrap/dist/css/bootstrap.min.css'
+        ],
         dist: './dist',
         mainJs: './src/app.js'
     }
@@ -52,10 +57,23 @@ gulp.task('js', function (done) {
     done();
 });
 
-gulp.task('watch', function () {
-    gulp.watch(config.paths.html, gulp.series('html'));
-    gulp.watch(config.paths.js, gulp.series('js'));
+gulp.task('css', function (done) {
+    gulp.src(config.paths.css)
+        .pipe(concat('bundle.css'))
+        .pipe(gulp.dest(config.paths.dist + '/css'));
+    done();
 });
 
-gulp.task('default', gulp.series('html', 'js', 'open', 'watch'));
+gulp.task('lint', function (done) {
+    return gulp.src(config.paths.js)
+        .pipe(eslint({ config: 'eslint.config.jsom' }))
+        .pipe(eslint.format());
+});
+
+gulp.task('watch', function () {
+    gulp.watch(config.paths.html, gulp.series('html'));
+    gulp.watch(config.paths.js, gulp.series(['js','lint']));
+});
+
+gulp.task('default', gulp.series('html', 'js', 'css', 'lint', 'open', 'watch'));
 
--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+var $, jQuery = require('jquery');
 var App = console.log('Test');
 
 module.exports = App;
